de63f3950333a7934b2cf88610eadcfac8487c0c
VishalTyagi8974 <115866008+VishalTyagi8974@users.noreply.github.com>
2024-05-31 14:33:29 +0530

    Update index.js google auth routes

--- index.js
+++ index.js
@@ -120,7 +120,9 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: process.env.NODE_ENV === 'production' 
+               ? "https://yelpcamp-i8mj.onrender.com/auth/google/callback"
+               : "http://localhost:3000/auth/google/callback"
 },
     async (accessToken, refreshToken, profile, done) => {
         try {
